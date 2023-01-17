cosi-project/runtime

commit 1ed3207ee2808e35ea0f3a6ddbe149ed7e90223b

feat: expose resource registry resource creation methods
Directly provide a method to create a resource instance from a registered
resource type.

Then it will be possible to decode the resource manually.
Also rerun Kres and fix all linter issues.

Signed-off-by: Artem Chernyshev <artem.chernyshev@talos-systems.com>

pkg/controller/protobuf/client/client.go
@@ -250,7 +250,9 @@ func (ctrlAdapter *controllerAdapter) run(ctx context.Context) {
 	}
 }
 
-func (ctrlAdapter *controllerAdapter) runOnce(ctx context.Context, logger *zap.Logger) (err error) {
+func (ctrlAdapter *controllerAdapter) runOnce(ctx context.Context, logger *zap.Logger) error {
+	var err error
+
 	defer func() {
 		if err != nil && (errors.Is(err, context.Canceled) || status.Code(errors.Unwrap(err)) == codes.Canceled) {
 			err = nil
@@ -273,7 +275,7 @@ func (ctrlAdapter *controllerAdapter) runOnce(ctx context.Context, logger *zap.L
 
 	err = ctrlAdapter.controller.Run(ctx, ctrlAdapter, logger)
 
-	return
+	return err
 }
 
 func (ctrlAdapter *controllerAdapter) establishEventChannel() {

pkg/controller/runtime/adapter.go
@@ -383,7 +383,9 @@ func (adapter *adapter) run(ctx context.Context) {
 	}
 }
 
-func (adapter *adapter) runOnce(ctx context.Context, logger *zap.Logger) (err error) {
+func (adapter *adapter) runOnce(ctx context.Context, logger *zap.Logger) error {
+	var err error
+
 	defer func() {
 		if err != nil && errors.Is(err, context.Canceled) {
 			err = nil
@@ -406,5 +408,5 @@ func (adapter *adapter) runOnce(ctx context.Context, logger *zap.Logger) (err er
 
 	err = adapter.ctrl.Run(ctx, adapter, logger)
 
-	return
+	return err
 }

.golangci.yml
@@ -1,6 +1,6 @@
 # THIS FILE WAS AUTOMATICALLY GENERATED, PLEASE DO NOT EDIT.
 #
-# Generated on 2022-04-12T21:42:27Z by kres 4975f30.
+# Generated on 2022-05-25T15:41:00Z by kres latest.
 
 # options for analysis running
 run:
@@ -124,6 +124,7 @@ linters:
   disable-all: false
   fast: false
   disable:
+    - exhaustruct
     - exhaustivestruct
     - forbidigo
     - funlen

.kres.yaml
@@ -1,5 +1,5 @@
 ---
-kind: golang.Protobuf
+kind: golang.Generate
 spec:
   experimentalFlags:
     - --experimental_allow_proto3_optional

Dockerfile
@@ -2,7 +2,7 @@
 
 # THIS FILE WAS AUTOMATICALLY GENERATED, PLEASE DO NOT EDIT.
 #
-# Generated on 2022-04-22T17:30:48Z by kres 685be7b-dirty.
+# Generated on 2022-05-25T15:53:03Z by kres latest.
 
 ARG TOOLCHAIN
 
@@ -11,13 +11,13 @@ FROM ghcr.io/siderolabs/ca-certificates:v1.0.0 AS image-ca-certificates
 FROM ghcr.io/siderolabs/fhs:v1.0.0 AS image-fhs
 
 # runs markdownlint
-FROM node:14.8.0-alpine AS lint-markdown
-RUN npm i -g markdownlint-cli@0.23.2
-RUN npm i sentences-per-line@0.2.1
+FROM node:18.2.0-alpine AS lint-markdown
 WORKDIR /src
+RUN npm i -g markdownlint-cli@0.31.1
+RUN npm i sentences-per-line@0.2.1
 COPY .markdownlint.json .
 COPY ./README.md ./README.md
-RUN markdownlint --ignore "CHANGELOG.md" --ignore "**/node_modules/**" --ignore '**/hack/chglog/**' --rules /node_modules/sentences-per-line/index.js .
+RUN markdownlint --ignore "CHANGELOG.md" --ignore "**/node_modules/**" --ignore '**/hack/chglog/**' --rules node_modules/sentences-per-line/index.js .
 
 # collects proto specs
 FROM scratch AS proto-specs
@@ -34,7 +34,8 @@ FROM toolchain AS tools
 ENV GO111MODULE on
 ENV CGO_ENABLED 0
 ENV GOPATH /go
-RUN curl -sfL https://install.goreleaser.com/github.com/golangci/golangci-lint.sh | bash -s -- -b /bin v1.45.2
+ARG GOLANGCILINT_VERSION
+RUN curl -sfL https://raw.githubusercontent.com/golangci/golangci-lint/${GOLANGCILINT_VERSION}/install.sh | bash -s -- -b /bin ${GOLANGCILINT_VERSION}
 ARG GOFUMPT_VERSION
 RUN go install mvdan.cc/gofumpt@${GOFUMPT_VERSION} \
 	&& mv /go/bin/gofumpt /bin/gofumpt
@@ -53,6 +54,9 @@ RUN mv /go/bin/protoc-gen-grpc-gateway /bin
 ARG VTPROTOBUF_VERSION
 RUN go install github.com/planetscale/vtprotobuf/cmd/protoc-gen-go-vtproto@v${VTPROTOBUF_VERSION}
 RUN mv /go/bin/protoc-gen-go-vtproto /bin
+ARG DEEPCOPY_VERSION
+RUN go install github.com/siderolabs/deep-copy@${DEEPCOPY_VERSION} \
+	&& mv /go/bin/deep-copy /bin/deep-copy
 
 # tools and sources
 FROM tools AS base

Makefile
@@ -1,6 +1,6 @@
 # THIS FILE WAS AUTOMATICALLY GENERATED, PLEASE DO NOT EDIT.
 #
-# Generated on 2022-04-22T17:29:34Z by kres 685be7b-dirty.
+# Generated on 2022-05-25T15:53:03Z by kres latest.
 
 # common variables
 
@@ -11,6 +11,7 @@ ARTIFACTS := _out
 REGISTRY ?= ghcr.io
 USERNAME ?= cosi-project
 REGISTRY_AND_USERNAME ?= $(REGISTRY)/$(USERNAME)
+GOLANGCILINT_VERSION ?= v1.46.2
 GOFUMPT_VERSION ?= v0.3.1
 GO_VERSION ?= 1.18
 GOIMPORTS_VERSION ?= v0.1.10
@@ -18,8 +19,10 @@ PROTOBUF_GO_VERSION ?= 1.28.0
 GRPC_GO_VERSION ?= 1.2.0
 GRPC_GATEWAY_VERSION ?= 2.10.0
 VTPROTOBUF_VERSION ?= 0.3.0
+DEEPCOPY_VERSION ?= v0.5.5
 TESTPKGS ?= ./...
 KRES_IMAGE ?= ghcr.io/siderolabs/kres:latest
+CONFORMANCE_IMAGE ?= ghcr.io/siderolabs/conform:latest
 
 # docker build settings
 
@@ -37,12 +40,14 @@ COMMON_ARGS += --build-arg=SHA=$(SHA)
 COMMON_ARGS += --build-arg=TAG=$(TAG)
 COMMON_ARGS += --build-arg=USERNAME=$(USERNAME)
 COMMON_ARGS += --build-arg=TOOLCHAIN=$(TOOLCHAIN)
+COMMON_ARGS += --build-arg=GOLANGCILINT_VERSION=$(GOLANGCILINT_VERSION)
 COMMON_ARGS += --build-arg=GOFUMPT_VERSION=$(GOFUMPT_VERSION)
 COMMON_ARGS += --build-arg=GOIMPORTS_VERSION=$(GOIMPORTS_VERSION)
 COMMON_ARGS += --build-arg=PROTOBUF_GO_VERSION=$(PROTOBUF_GO_VERSION)
 COMMON_ARGS += --build-arg=GRPC_GO_VERSION=$(GRPC_GO_VERSION)
 COMMON_ARGS += --build-arg=GRPC_GATEWAY_VERSION=$(GRPC_GATEWAY_VERSION)
 COMMON_ARGS += --build-arg=VTPROTOBUF_VERSION=$(VTPROTOBUF_VERSION)
+COMMON_ARGS += --build-arg=DEEPCOPY_VERSION=$(DEEPCOPY_VERSION)
 COMMON_ARGS += --build-arg=TESTPKGS=$(TESTPKGS)
 TOOLCHAIN ?= docker.io/golang:1.18-alpine
 
@@ -161,4 +166,9 @@ help:  ## This help menu.
 release-notes:
 	mkdir -p $(ARTIFACTS)
 	@ARTIFACTS=$(ARTIFACTS) ./hack/release.sh $@ $(ARTIFACTS)/RELEASE_NOTES.md $(TAG)
+
+.PHONY: conformance
+conformance:
+	@docker pull $(CONFORMANCE_IMAGE)
+	@docker run --rm -it -v $(PWD):/src -w /src $(CONFORMANCE_IMAGE) enforce
 

pkg/controller/protobuf/protobuf_test.go
@@ -73,7 +73,7 @@ func TestProtobufConformance(t *testing.T) {
 			suite.grpcServer.Serve(l) //nolint:errcheck
 		}()
 
-		suite.grpcConn, err = grpc.Dial("unix://"+suite.sock.Name(), grpc.WithInsecure())
+		suite.grpcConn, err = grpc.Dial("unix://"+suite.sock.Name(), grpc.WithInsecure()) //nolint:staticcheck
 		require.NoError(t, err)
 
 		stateClient := v1alpha1.NewStateClient(suite.grpcConn)

pkg/resource/finalizer.go
@@ -13,7 +13,7 @@ type Finalizer = string
 type Finalizers []Finalizer
 
 // Add a (unique) Finalizer to the set.
-func (fins *Finalizers) Add(fin Finalizer) (added bool) {
+func (fins *Finalizers) Add(fin Finalizer) bool {
 	*fins = append(Finalizers(nil), *fins...)
 
 	for _, f := range *fins {
@@ -28,20 +28,18 @@ func (fins *Finalizers) Add(fin Finalizer) (added bool) {
 }
 
 // Remove a (unique) Finalizer from the set.
-func (fins *Finalizers) Remove(fin Finalizer) (removed bool) {
+func (fins *Finalizers) Remove(fin Finalizer) bool {
 	*fins = append(Finalizers(nil), *fins...)
 
 	for i, f := range *fins {
 		if f == fin {
-			removed = true
-
 			*fins = append((*fins)[:i], (*fins)[i+1:]...)
 
-			return
+			return true
 		}
 	}
 
-	return
+	return false
 }
 
 // Empty returns true if list of finalizers is empty.

commit 62c39680d91b09d457e4c3a345e238e744cb6799

fix: restart controllers on panic
This feature got broken in 1ed3207ee2808e35ea0f3a6ddbe149ed7e90223b,
as named return was replaced with a variable which doesn't work to
override return value in defer blocks.

In case of panic, panic was intercepted correctly, but still `nil` was
returned, as panic was handled. And `nil` return means "don't restart"
which stops automatic controller restarts.

Signed-off-by: Andrey Smirnov <andrey.smirnov@talos-systems.com>

pkg/controller/conformance/controllers.go
@@ -306,6 +306,7 @@ func (ctrl *SumController) Run(ctx context.Context, r controller.Runtime, logger
 // FailingController fails on each iteration creating new resources each time.
 type FailingController struct {
 	TargetNamespace resource.Namespace
+	Panic           bool
 
 	count int
 }
@@ -347,6 +348,10 @@ func (ctrl *FailingController) Run(ctx context.Context, r controller.Runtime, lo
 	}
 
 	ctrl.count++
+
+	if ctrl.Panic {
+		panic("failing here")
+	}
 
 	return fmt.Errorf("failing here")
 }

pkg/controller/conformance/runtime.go
@@ -400,3 +400,19 @@ func (suite *RuntimeSuite) TestFailingController() {
 	suite.Assert().NoError(retry.Constant(5*time.Second, retry.WithUnits(10*time.Millisecond)).
 		Retry(suite.assertIntObjects([]string{"0", "1"}, []int{0, 1})))
 }
+
+// TestPanickingController ...
+func (suite *RuntimeSuite) TestPanickingController() {
+	suite.Require().NoError(suite.Runtime.RegisterController(&FailingController{
+		TargetNamespace: "target",
+		Panic:           true,
+	}))
+
+	suite.startRuntime()
+
+	suite.Assert().NoError(retry.Constant(5*time.Second, retry.WithUnits(10*time.Millisecond)).
+		Retry(suite.assertIntObjects([]string{"0"}, []int{0})))
+
+	suite.Assert().NoError(retry.Constant(5*time.Second, retry.WithUnits(10*time.Millisecond)).
+		Retry(suite.assertIntObjects([]string{"0", "1"}, []int{0, 1})))
+}

pkg/controller/protobuf/client/client.go
@@ -252,9 +252,7 @@ func (ctrlAdapter *controllerAdapter) run(ctx context.Context) {
 	}
 }
 
-func (ctrlAdapter *controllerAdapter) runOnce(ctx context.Context, logger *zap.Logger) error {
-	var err error
-
+func (ctrlAdapter *controllerAdapter) runOnce(ctx context.Context, logger *zap.Logger) (err error) {
 	defer func() {
 		if err != nil && (errors.Is(err, context.Canceled) || status.Code(errors.Unwrap(err)) == codes.Canceled) {
 			err = nil
@@ -275,9 +273,7 @@ func (ctrlAdapter *controllerAdapter) runOnce(ctx context.Context, logger *zap.L
 
 	logger.Debug("controller starting")
 
-	err = ctrlAdapter.controller.Run(ctx, ctrlAdapter, logger)
-
-	return err
+	return ctrlAdapter.controller.Run(ctx, ctrlAdapter, logger)
 }
 
 func (ctrlAdapter *controllerAdapter) establishEventChannel() {

pkg/controller/runtime/adapter.go
@@ -418,9 +418,7 @@ func (adapter *adapter) run(ctx context.Context) {
 	}
 }
 
-func (adapter *adapter) runOnce(ctx context.Context, logger *zap.Logger) error {
-	var err error
-
+func (adapter *adapter) runOnce(ctx context.Context, logger *zap.Logger) (err error) {
 	defer func() {
 		if err != nil && errors.Is(err, context.Canceled) {
 			err = nil
@@ -441,7 +439,5 @@ func (adapter *adapter) runOnce(ctx context.Context, logger *zap.Logger) error {
 
 	logger.Debug("controller starting")
 
-	err = adapter.ctrl.Run(ctx, adapter, logger)
-
-	return err
+	return adapter.ctrl.Run(ctx, adapter, logger)
 }